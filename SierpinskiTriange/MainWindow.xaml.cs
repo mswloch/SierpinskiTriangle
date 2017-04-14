d88ff9dd2c8c3e0f29249a3c437c249ce5418098
Marcin <mswloch@outlook.com>
2017-04-14 11:55:36 +0200

    init

---
+++ SierpinskiTriange/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SierpinskiTriange
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private SolidColorBrush brush = new SolidColorBrush();
+      
+        public MainWindow()
+        {
+            InitializeComponent();
+            brush.Color = Color.FromArgb(255, 255, 0, 0);
+
+            Draw();
+        }
+
+        /// <summary>
+        /// drawing triangle with lines
+        /// </summary>
+        /// <param name="t">triangle model</param>
+        public void DrawTriangle(Triangle t)
+        {            
+            DrawLine(t.a, t.b);
+            DrawLine(t.b, t.c);
+            DrawLine(t.c, t.a);
+        }
+
+        /// <summary>
+        /// Draw simulation on "canvas" grid panel
+        /// </summary>
+        public void Draw()
+        {
+            Triangle MainTriangle = new Triangle(new Point(0, 100), Canvas.Width);
+            MainTriangle.Recurse(6);
+
+            List<Triangle> list = MainTriangle.GetRecurseTriangles();
+
+            list.ForEach(x => DrawTriangle(x));
+        }
+
+        /// <summary>
+        /// simplifier for line drawing
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public void DrawLine(Point a, Point b)
+        {
+            Line line = new Line();
+            line.Fill = brush;
+            line.Stroke = Brushes.Black;
+            line.StrokeThickness = 0.5;
+
+            line.X1 = a.X;
+            line.Y1 = Canvas.Width - a.Y;
+            line.X2 = b.X;
+            line.Y2 = Canvas.Width - b.Y;
+
+            Canvas.Children.Add(line);
+        }
+    }
+}
---
+++ SierpinskiTriange/Triangle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SierpinskiTriange
+{
+    public class Triangle
+    {
+        public Point a;
+        public Point b;
+        public Point c;
+
+        List<Triangle> triangles;
+        private double Length;
+
+        public Triangle(Point begin, double length)
+        {
+            triangles = new List<Triangle>();
+            Length = length;
+
+            a = begin;
+            b = new Point(a.X + Length, a.Y);
+            c = new Point(a.X + Length / 2, a.Y + (Length * Math.Sqrt(3) / 2));            
+        }
+
+        /// <summary>
+        /// Calculating smaller triangles
+        /// </summary>
+        public void AddTriangles()
+        {
+            Triangle t1 = new Triangle(a, Length/2);
+            Triangle t2 = new Triangle(new Point(a.X+(Length/2),a.Y), Length/2);
+            Triangle t3 = new Triangle(new Point((a.X+c.X)/2,(a.Y+c.Y)/2), Length / 2);
+
+            triangles = new List<Triangle>() { t1, t2, t3 };
+        }
+
+        /// <summary>
+        /// Recurse method for generating new triangles
+        /// </summary>
+        /// <param name="i">Recurse index, recursion counter</param>
+        public void Recurse(int i)
+        {
+            AddTriangles();
+            if (i != 0)
+                triangles.ForEach(x => x.Recurse(i-1));
+        }
+
+        /// <summary>
+        /// Getting all triangles from tree
+        /// </summary>
+        /// <returns></returns>
+        public List<Triangle> GetRecurseTriangles()
+        {
+            List<Triangle> list = new List<Triangle>();
+            
+            list.AddRange(triangles);
+            triangles.ForEach(x => list.AddRange(x.GetRecurseTriangles()));
+
+            return list;
+        }
+    }
+}
